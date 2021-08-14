44d58540bf3bdea9291ca37f64a58ac6f9fa8f16
Adriano <adriano.soares@glintt.com>
2021-08-14 19:14:28 +0100

    start udemy course

---
+++ Rocky/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; }
+
+        public string Name { get; }
+
+        public int DisplayOrder { get; }
+    }
+}
